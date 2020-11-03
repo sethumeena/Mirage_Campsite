eb46a3c87b7031597a824d0447b517dcc4257827
Sethu_Meena <sethumeenakaruppiah@gmail.com>
2020-11-03 18:20:40 -0500

    Updated

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ const barTwo = document.querySelector('.bar-two');
 const barThree = document.querySelector('.bar-three');
 
 hamburgerMenu.addEventListener('click', ()=>{
+   
    navList.classList.toggle('open');
    barOne.classList.toggle('change');
    barTwo.classList.toggle('change');
    barThree.classList.toggle('change');
    
 });
-const value = 'b'
 
-if(value == 'a' || 'b'){
-   console.log(value)
-}
+
+
